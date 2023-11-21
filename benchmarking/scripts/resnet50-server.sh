afb8051b065e1e7a755294f978302df40a8a8f32
Mohamadol <mohamadolyaiy@client.europe-west2-c.c.rock-appliance-396012.internal>
2023-11-21 07:15:40 +0000

    added scripts

---
+++ benchmarking/scripts/resnet50-server.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Program name or path
+PROGRAM="/mnt/mohammad/delphi/rust/target/release/resnet50-server"
+
+# Start each instance in the background
+for i in {1..8}
+do
+   echo "Starting instance $i in the background"
+   $PROGRAM $i &  # Start in the background
+done
+
+# Wait for all background jobs to finish
+wait
+
+echo
