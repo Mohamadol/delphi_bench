773d5a04cca91a37f2d034e0eeb685ee90b0320f
Mohamadol <parinazzhandy@client.us-east1-c.c.decent-courage-412912.internal>
2024-03-19 22:09:58 +0000

    cifar10_throughput _32_32_120_120 1x3_batch client

--- benchmarking/scripts/resnet18-client_throughput.sh
+++ benchmarking/scripts/resnet18-client_throughput.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+BATCH=3
+MEMORY=120
+CORES=32
 network="resnet18"
 EXTRA="_throughput"
 EXP_NAME="${network}${EXTRA}"
 PROGRAM="/mnt/mohammad/delphi_bench/target/release/${network}-client"
-OUTDIR="/mnt/mohammad/delphi_bench/benchmarking/outputs/${EXP_NAME}/client"
-DATADIR="/mnt/mohammad/delphi_bench/benchmarking/data/${EXP_NAME}/client"
+OUTDIR="/mnt/mohammad/delphi_bench/benchmarking/outputs/${EXP_NAME}/_${CORES}_${CORES}_${MEMORY}_${MEMORY}/_${BATCH}__batch/client"
+DATADIR="/mnt/mohammad/delphi_bench/benchmarking/data/${EXP_NAME}/_${CORES}_${CORES}_${MEMORY}_${MEMORY}/_${BATCH}__batch/client"
 mkdir -p $OUTDIR
 mkdir -p $DATADIR
-BATCH=3
 
 
 start=$(date +%s%N)
@@ -16,7 +18,7 @@ pids=()
 for ((i=1; i<=BATCH; i++))
 do
     echo "Starting client for batch $i"
-    $PROGRAM "$i" "$EXP_NAME" > "${OUTDIR}/_batch_${i}.out" &
+    $PROGRAM "$i" "$EXP_NAME" "$CORES" "$MEMORY" "$BATCH" > "${OUTDIR}/_batch_${i}.out" &
     pids+=($!)
 done
 
