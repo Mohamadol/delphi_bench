cfa81e54ebff40621c5e87111a10c7a5b1ccb2f4
Mohamadol <parinazzhandy@client.us-central1-c.c.decent-courage-412912.internal>
2024-03-28 20:15:34 +0000

    swap config: automatically write to fstab

--- swap_configuration.sh
+++ swap_configuration.sh
@@ -3,7 +3,7 @@ sudo fallocate -l 64G /mnt/mohammad/swap/swapfile
 sudo chmod 600 /mnt/mohammad/swap/swapfile
 sudo mkswap /mnt/mohammad/swap/swapfile
 sudo swapon /mnt/mohammad/swap/swapfile
-sudo nano /etc/fstab
+echo '/mnt/mohammad/swap/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
 /mnt/mohammad/swap/swapfile none swap sw 0 0
 sudo swapon --show
 getconf PAGESIZE
