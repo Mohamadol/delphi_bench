58278034525f3a727df0b13ed4f9d01f84e96393
Mohamadol <parinazzhandy@client-l.us-east1-b.c.decent-courage-412912.internal>
2024-05-16 20:53:08 +0000

    large cifar latency server

--- experiments/src/latency/resnet18/client.rs
+++ experiments/src/latency/resnet18/client.rs
@@ -49,7 +49,7 @@ fn main() {
 
     let vs = tch::nn::VarStore::new(tch::Device::cuda_if_available());
     let mut rng = ChaChaRng::from_seed(RANDOMNESS);
-    let server_addr = format!("10.128.0.51:{}", 8001 + batch_id);
+    let server_addr = format!("10.128.0.62:{}", 8001 + batch_id);
     let network = construct_resnet18_model(Some(&vs.root()), 8, &mut rng);
 
     println!(
